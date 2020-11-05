6a5e872393b418a76bf42aa8db1d1dcb73a8214c
swolepenguin <ashtonm223@gmail.com>
2020-11-05 16:58:34 -0500

    setting up obj

--- app.js
+++ app.js
@@ -1,11 +1,42 @@
-const table = document.getElementsByClassName('table');
-const context = canvas.getcontext('2d');
-context.fillStyle = 'red';
-context.fillRect(100,200,50,75);
-context.fillStyle = 'orange';
-context.beginPath();
-context.arc(300,350,100,0, Math.PI*2, false);
-context.closePath();
-context.fill();
+const canvas = document.getElementById("table");
+const ctx = canvas.getContext('2d');
 
-function box()
+const ball = {
+    x : canvas.width/2,
+    y : canvas.height/2,
+    radius : 10,
+    velocityX : 5,
+    velocityY : 5,
+    speed : 7,
+    color : "blue"
+}
+
+
+const user = {
+    x : 0,
+    y : (canvas.height - 100)/2, 
+    width : 10,
+    height : 100,
+    score : 0,
+    color : "orange"
+}
+
+
+const com = {
+    x : canvas.width - 10, 
+    y : (canvas.height - 100)/2,
+    width : 10,
+    height : 100,
+    score : 0,
+    color : "orange"
+}
+
+
+const net = {
+    x : (canvas.width - 2)/2,
+    y : 0,
+    height : 10,
+    width : 2,
+    color : "purple"
+}
+
